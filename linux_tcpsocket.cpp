026b07ef6cea592c719c5869256df6f2c0dc6151
yychrzh <31843105+yychrzh@users.noreply.github.com>
2018-05-25 19:47:47 +0800

    commit

--- linux_tcpsocket.cpp
+++ linux_tcpsocket.cpp
@@ -1,72 +1,93 @@
 #include "linux_tcpsocket.h"
 
-void debug_print(char *info)
+
+tcpsocket::tcpsocket(string s_type="server", int port_n=8088, int buff_s=200, char *s_ip="127.0.0.1", bool debug=true)
 {
-#ifdef DEBUG_PRINT
-    printf("(debug)");
-    printf(info);
-#endif
+    // copy parameters
+	::socket_type = s_type
+    ::port_num = port_n;
+    ::buffsize = buff_s;
+    strcpy(::server_ip, s_ip);
+    ::debug_print_flag = debug;
+
+    if (::socket_type == "server"){
+        conn = ::create_conn_server();
+    }
+    else if (s_type == "client"){
+        conn = ::create_conn_client();
+    }
+    else{
+        ::debug_print("socket type input error!");
+    }
+    return;
 }
 
-// create a connenct server + recv connect_flag:
-int create_conn_server()
+tcpsocket::debug_print(string info)
+{
+    if (::debug_print_flag){
+        printf("(debug)");
+        printf("%s", info.c_str());
+    }
+	return;
+}
+
+tcpsocket::create_conn_server()
 {
 	struct sockaddr_in server_addr;          //save the server's socket address
     struct sockaddr_in client_addr;          //save the client's socket address
 	int sock_server, conn, err;
-  
-    /*****************socket()***************/  
-    sock_server = socket(AF_INET, SOCK_STREAM, 0);                    
+
+    /*****************socket()***************/
+    sock_server = socket(AF_INET, SOCK_STREAM, 0);
     if (sock_server < 0){
-        debug_print("server : server socket create error\n");
-        return -1;  
+        ::debug_print("server : server socket create error\n");
+        return -1;
     }
-  
-    /******************bind()****************/  
+
+    /******************bind()****************/
     //init the address struct
-	memset(&server_addr, 0, sizeof(server_addr));   
-    server_addr.sin_family = AF_INET;                   
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);   
-    server_addr.sin_port = htons(TCP_PORT);  
-  
-    err = bind(sock_server, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));  
+	memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(::port_num);
+
+    err = bind(sock_server, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
     if (err < 0){
-        debug_print("server : bind error\n");
-        return -1;  
-    }  
-  
-    /*****************listen()***************/  
-    err = listen(sock_server, BACKLOG);   //start listen from client 
+        ::debug_print("server : bind error\n");
+        return -1;
+    }
+
+    /*****************listen()***************/
+    err = listen(sock_server, BACKLOG);   //start listen from client
     if (err < 0){
-        debug_print("server : listen error\n");
-        return -1;  
-    }  
+        ::debug_print("server : listen error\n");
+        return -1;
+    }
 
 	/*****************accept()***************/
 	socklen_t addrlen = sizeof(client_addr);
 	// accept return the socket of client
-	conn = accept(sock_server, (struct sockaddr *)&client_addr, &addrlen);  
+	conn = accept(sock_server, (struct sockaddr *)&client_addr, &addrlen);
 	if (conn < 0){
-        debug_print("failed to accept from client!\n");
+        ::debug_print("failed to accept from client!\n");
 		return -1;
 	}
 	else{
-        debug_print("server : connected\n");
+        ::debug_print("server : connected\n");
 		return conn;
 	}
 }
 
-// create a connenct client with socket:
-int create_conn_client()
+tcpsocket::create_conn_client()
 {
-	sockaddr_in server_addr;
+    sockaddr_in server_addr;
 	int err, conn;
 	sighandler_t ret;
 
 	/********************socket()*********************/
 	conn = socket(AF_INET, SOCK_STREAM, 0);
 	if (conn < 0){
-        debug_print("client : create socket error\n");
+        ::debug_print("client : create socket error\n");
 		return -1;
 	}
 
@@ -76,15 +97,27 @@ int create_conn_client()
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(TCP_PORT);
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
-	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // inet_addr(SERVER_IP);
+	server_addr.sin_addr.s_addr = inet_addr(::server_ip); // inet_addr(SERVER_IP);
 
 	err = connect(conn, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
 	if (err == 0){
-        debug_print("client : connect to server success\n");
+        ::debug_print("client : connect to server success\n");
         return conn;
 	}
 	else{
-        debug_print("client : connect error\n");
+        ::debug_print("client : connect error\n");
 		return -1;
 	}
+}
+
+// recv_buf: char pointer that stores the received strings
+tcpsocket::recv(char *recv_buf)
+{
+	recv(::conn, recv_buf, ::buffsize, 0)
+}
+
+// send_buf: char pointer that stores the strings will be sent
+tcpsocket::send(char *send_buf, int send_data_lens)
+{
+	send(::conn, se, ::buffsize, 0)
 }
--- linux_tcpsocket.h
+++ linux_tcpsocket.h
@@ -2,11 +2,11 @@
 #define LINUX_TCPSOCKET_H_
 
 #include <stdio.h>  
-#include <strings.h>  
+#include <strings.h>
+#include <stdlib.h>
 #include <unistd.h>  
 #include <sys/types.h>  
-#include <sys/socket.h>  
-#include <stdlib.h>  
+#include <sys/socket.h>
 #include <memory.h>  
 #include <arpa/inet.h>  
 #include <netinet/in.h>  
@@ -15,16 +15,31 @@
 typedef unsigned char BYTE;
 #endif
 
-#define TCP_PORT              11910             //define commuicate port
 #define BACKLOG               5                   
-#define BUFFSIZE              1024  
-#define SERVER_IP             "127.0.0.1"
-#define DEBUG_PRINT           1
 
 void debug_print(char *info);
 int create_conn_server();
 int create_conn_client();
 int test_server(int conn);
 int test_client(int conn);
+
+
+// a class for socket communication
+class tcpsocket
+{
+    public:
+        tcpsocket(string s_type='server', int port_n=8088, int buff_s=200, char *s_ip='127.0.0.1', bool debug=true);
+        void send(char *send_buf, int send_data_lens);
+        void recv(char *recv_buf);
+        void debug_print(string info);
+    private:
+	    string socket_type;
+	    int port;
+	    int buffsize;
+	    char server_ip[50];
+	    bool debug_print_flag;
+	    int create_conn_server();
+        int create_conn_client();
+};
 
 #endif
